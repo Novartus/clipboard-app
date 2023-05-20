5f3862badc031d557f0cf58e90c19ccec4c0f31b
Abhee Hudani <hudaniabhee@gmail.com>
2023-05-20 17:34:22 -0400

    Feature: Loader added, second-instance restore mainWindow fun added

--- public/main.js
+++ public/main.js
@@ -1,30 +1,53 @@
 const electron = require("electron");
-// const path = require("path");
+const path = require("path");
 
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
 let mainWindow;
+let loadingWindow;
 
-function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: { nodeIntegration: true, contextIsolation: false },
+app.on("ready", () => {
+  loadingWindow = new BrowserWindow({
+    frame: false,
+    movable: false,
+    scrollBounce: false,
+    autoHideMenuBar: true,
+    overlayScrollbars: false,
   });
 
-  console.log(__dirname);
-  mainWindow.loadURL("http://localhost:3000/");
-  // mainWindow.loadFile(path.join(__dirname, "../build/index.html"));
-}
+  loadingWindow.once("show", () => {
+    mainWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: { nodeIntegration: true, contextIsolation: false },
+      show: false,
+      scrollBounce: false,
+      autoHideMenuBar: true,
+      overlayScrollbars: false,
+    });
 
-app.whenReady().then(() => {
-  createWindow();
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
+    mainWindow.webContents.once("dom-ready", () => {
+      mainWindow.show();
+      loadingWindow.hide();
+      loadingWindow.close();
+    });
+    const icon = path.join(__dirname, "./clipboard.ico");
+    mainWindow.setIcon(icon);
+    mainWindow.loadURL("http://localhost:3000/");
+  });
+
+  loadingWindow.loadFile(path.join(__dirname, "./loader.html"));
+  loadingWindow.show();
+});
+
+app.on("second-instance", () => {
+  if (mainWindow) {
+    if (mainWindow.isMinimized()) {
+      mainWindow.restore();
     }
-  });
+    mainWindow.focus();
+  }
 });
 
 app.on("window-all-closed", function () {
